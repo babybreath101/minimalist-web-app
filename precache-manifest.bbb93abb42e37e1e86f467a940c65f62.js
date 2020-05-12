c837da368a335f9bdbc45954e49145bbebb3af45
Cintya Agusti Anindita <cintyaagusti99@mail.ugm.ac.id>
2020-05-13 05:27:52 +0700

    Updates

---
+++ precache-manifest.bbb93abb42e37e1e86f467a940c65f62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8ec6e0e607ee19574bc56c5f1d47fd",
+    "url": "/minimalist-web-app/index.html"
+  },
+  {
+    "revision": "0b3f856aef69a6523a59",
+    "url": "/minimalist-web-app/static/css/2.2fbe55bd.chunk.css"
+  },
+  {
+    "revision": "dbcd0ecb12a77faf2c1b",
+    "url": "/minimalist-web-app/static/css/main.2236a508.chunk.css"
+  },
+  {
+    "revision": "0b3f856aef69a6523a59",
+    "url": "/minimalist-web-app/static/js/2.4226e357.chunk.js"
+  },
+  {
+    "revision": "88a28bf9811004a7250f8b652924c27d",
+    "url": "/minimalist-web-app/static/js/2.4226e357.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbcd0ecb12a77faf2c1b",
+    "url": "/minimalist-web-app/static/js/main.c4ec5289.chunk.js"
+  },
+  {
+    "revision": "531ec9517b8464be9323",
+    "url": "/minimalist-web-app/static/js/runtime-main.3c81a47e.js"
+  }
+]);
